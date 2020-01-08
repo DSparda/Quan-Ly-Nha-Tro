a6eb5c6899d58acca240b8011354a12cfeef9823
DSparda <DSparda@users.noreply.github.com>
2020-01-08 09:32:30 +0700

    Add change color and bill value rotation for button

--- app/src/main/java/com/example/boardinghousemanage/screens/title/TitleFragment.kt
+++ app/src/main/java/com/example/boardinghousemanage/screens/title/TitleFragment.kt
@@ -60,10 +60,178 @@ class TitleFragment : Fragment() {
         })
 
         binding.apply {
+            buttonTang11.setOnLongClickListener {
+                viewModel.p101State?.let {
+                    if (viewModel.p101State.value == 3) {
+                        viewModel.longPress(101, 2)
+                    }
+                }
+                true
+            }
             buttonTang12.setOnLongClickListener {
                 viewModel.p102State?.let {
                     if (viewModel.p102State.value == 3) {
                         viewModel.longPress(102, 2)
+                    }
+                }
+                true
+            }
+            buttonTang13.setOnLongClickListener {
+                viewModel.p103State?.let {
+                    if (viewModel.p103State.value == 3) {
+                        viewModel.longPress(103, 2)
+                    }
+                }
+                true
+            }
+            buttonTang14.setOnLongClickListener {
+                viewModel.p104State?.let {
+                    if (viewModel.p104State.value == 3) {
+                        viewModel.longPress(104, 2)
+                    }
+                }
+                true
+            }
+            buttonTang15.setOnLongClickListener {
+                viewModel.p105State?.let {
+                    if (viewModel.p105State.value == 3) {
+                        viewModel.longPress(105, 2)
+                    }
+                }
+                true
+            }
+            buttonTang21.setOnLongClickListener {
+                viewModel.p201State?.let {
+                    if (viewModel.p201State.value == 3) {
+                        viewModel.longPress(201, 2)
+                    }
+                }
+                true
+            }
+            buttonTang22.setOnLongClickListener {
+                viewModel.p202State?.let {
+                    if (viewModel.p202State.value == 3) {
+                        viewModel.longPress(202, 2)
+                    }
+                }
+                true
+            }
+            buttonTang23.setOnLongClickListener {
+                viewModel.p203State?.let {
+                    if (viewModel.p203State.value == 3) {
+                        viewModel.longPress(203, 2)
+                    }
+                }
+                true
+            }
+            buttonTang24.setOnLongClickListener {
+                viewModel.p204State?.let {
+                    if (viewModel.p204State.value == 3) {
+                        viewModel.longPress(204, 2)
+                    }
+                }
+                true
+            }
+            buttonTang25.setOnLongClickListener {
+                viewModel.p205State?.let {
+                    if (viewModel.p205State.value == 3) {
+                        viewModel.longPress(205, 2)
+                    }
+                }
+                true
+            }
+            buttonTang31.setOnLongClickListener {
+                viewModel.p301State?.let {
+                    if (viewModel.p301State.value == 3) {
+                        viewModel.longPress(301, 2)
+                    }
+                }
+                true
+            }
+            buttonTang32.setOnLongClickListener {
+                viewModel.p302State?.let {
+                    if (viewModel.p302State.value == 3) {
+                        viewModel.longPress(302, 2)
+                    }
+                }
+                true
+            }
+            buttonTang33.setOnLongClickListener {
+                viewModel.p303State?.let {
+                    if (viewModel.p303State.value == 3) {
+                        viewModel.longPress(303, 2)
+                    }
+                }
+                true
+            }
+            buttonTang34.setOnLongClickListener {
+                viewModel.p304State?.let {
+                    if (viewModel.p304State.value == 3) {
+                        viewModel.longPress(304, 2)
+                    }
+                }
+                true
+            }
+            buttonTang35.setOnLongClickListener {
+                viewModel.p305State?.let {
+                    if (viewModel.p305State.value == 3) {
+                        viewModel.longPress(305, 2)
+                    }
+                }
+                true
+            }
+            buttonTang41.setOnLongClickListener {
+                viewModel.p401State?.let {
+                    if (viewModel.p401State.value == 3) {
+                        viewModel.longPress(401, 2)
+                    }
+                }
+                true
+            }
+            buttonTang42.setOnLongClickListener {
+                viewModel.p402State?.let {
+                    if (viewModel.p402State.value == 3) {
+                        viewModel.longPress(402, 2)
+                    }
+                }
+                true
+            }
+            buttonTang43.setOnLongClickListener {
+                viewModel.p403State?.let {
+                    if (viewModel.p403State.value == 3) {
+                        viewModel.longPress(403, 2)
+                    }
+                }
+                true
+            }
+            buttonTang44.setOnLongClickListener {
+                viewModel.p404State?.let {
+                    if (viewModel.p404State.value == 3) {
+                        viewModel.longPress(404, 2)
+                    }
+                }
+                true
+            }
+            buttonTang45.setOnLongClickListener {
+                viewModel.p405State?.let {
+                    if (viewModel.p405State.value == 3) {
+                        viewModel.longPress(405, 2)
+                    }
+                }
+                true
+            }
+            buttonTang51.setOnLongClickListener {
+                viewModel.p501State?.let {
+                    if (viewModel.p501State.value == 3) {
+                        viewModel.longPress(501, 2)
+                    }
+                }
+                true
+            }
+            buttonTang52.setOnLongClickListener {
+                viewModel.p502State?.let {
+                    if (viewModel.p502State.value == 3) {
+                        viewModel.longPress(502, 2)
                     }
                 }
                 true
--- app/src/main/java/com/example/boardinghousemanage/screens/title/TitleViewModel.kt
+++ app/src/main/java/com/example/boardinghousemanage/screens/title/TitleViewModel.kt
@@ -6,15 +6,15 @@ import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.Button
 import androidx.core.graphics.drawable.toDrawable
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.*
+import com.example.boardinghousemanage.database.PhieuThuDao
 import com.example.boardinghousemanage.database.PhongDao
 import kotlinx.coroutines.*
+import java.util.*
 
 class TitleViewModel(
     val database: PhongDao,
+    val databasePT: PhieuThuDao,
     application: Application
 ) : AndroidViewModel(application) {
 
@@ -45,7 +45,466 @@ class TitleViewModel(
     val p501State = database.getState(501)
     val p502State = database.getState(502)
 
-    fun longPress(key: Int, state :Int) {
+
+    val tong101 = databasePT.getTong(101)
+    val tong102 = databasePT.getTong(102)
+    val tong103 = databasePT.getTong(103)
+    val tong104 = databasePT.getTong(104)
+    val tong105 = databasePT.getTong(105)
+    val tong201 = databasePT.getTong(201)
+    val tong202 = databasePT.getTong(202)
+    val tong203 = databasePT.getTong(203)
+    val tong204 = databasePT.getTong(204)
+    val tong205 = databasePT.getTong(205)
+    val tong301 = databasePT.getTong(301)
+    val tong302 = databasePT.getTong(302)
+    val tong303 = databasePT.getTong(303)
+    val tong304 = databasePT.getTong(304)
+    val tong305 = databasePT.getTong(305)
+    val tong401 = databasePT.getTong(401)
+    val tong402 = databasePT.getTong(402)
+    val tong403 = databasePT.getTong(403)
+    val tong404 = databasePT.getTong(404)
+    val tong405 = databasePT.getTong(405)
+    val tong501 = databasePT.getTong(501)
+    val tong502 = databasePT.getTong(502)
+
+
+    val checkDebt = MutableLiveData<Boolean>()
+    fun checkLastest(liveData1: LiveData<Int>): Int {
+        val check1 = liveData1.value
+        if (check1 == null)
+            return 0
+        return check1
+    }
+
+
+    fun getTienTong101(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong101) { value ->
+            if (p101State.value == 3)
+                tienTong.value = checkLastest(tong101)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p101State.value == 3) tienTong.value =
+                checkLastest(tong101)
+            else tienTong.value = 101
+        }
+        return tienTong
+    }
+
+    fun getTienTong102(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong102) { value ->
+            if (p102State.value == 3)
+                tienTong.value = checkLastest(tong102)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p102State.value == 3) tienTong.value =
+                checkLastest(tong102)
+            else tienTong.value = 102
+        }
+        return tienTong
+    }
+
+    fun getTienTong103(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong103) { value ->
+            if (p103State.value == 3)
+                tienTong.value = checkLastest(tong103)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p103State.value == 3) tienTong.value =
+                checkLastest(tong103)
+            else tienTong.value = 103
+        }
+        return tienTong
+    }
+
+    fun getTienTong104(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong104) { value ->
+            if (p104State.value == 3)
+                tienTong.value = checkLastest(tong104)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p104State.value == 3) tienTong.value =
+                checkLastest(tong104)
+            else tienTong.value = 104
+        }
+        return tienTong
+    }
+
+    fun getTienTong105(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong105) { value ->
+            if (p105State.value == 3)
+                tienTong.value = checkLastest(tong105)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p105State.value == 3) tienTong.value =
+                checkLastest(tong105)
+            else tienTong.value = 105
+        }
+        return tienTong
+    }
+
+    fun getTienTong201(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong201) { value ->
+            if (p201State.value == 3)
+                tienTong.value = checkLastest(tong201)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p201State.value == 3) tienTong.value =
+                checkLastest(tong201)
+            else tienTong.value = 201
+        }
+        return tienTong
+    }
+
+    fun getTienTong202(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong202) { value ->
+            if (p202State.value == 3)
+                tienTong.value = checkLastest(tong202)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p202State.value == 3) tienTong.value =
+                checkLastest(tong202)
+            else tienTong.value = 202
+        }
+        return tienTong
+    }
+
+    fun getTienTong203(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong203) { value ->
+            if (p203State.value == 3)
+                tienTong.value = checkLastest(tong203)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p203State.value == 3) tienTong.value =
+                checkLastest(tong203)
+            else tienTong.value = 203
+        }
+        return tienTong
+    }
+
+    fun getTienTong204(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong204) { value ->
+            if (p204State.value == 3)
+                tienTong.value = checkLastest(tong204)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p204State.value == 3) tienTong.value =
+                checkLastest(tong204)
+            else tienTong.value = 204
+        }
+        return tienTong
+    }
+
+    fun getTienTong205(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong205) { value ->
+            if (p205State.value == 3)
+                tienTong.value = checkLastest(tong205)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p205State.value == 3) tienTong.value =
+                checkLastest(tong205)
+            else tienTong.value = 205
+        }
+        return tienTong
+    }
+
+    fun getTienTong301(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong301) { value ->
+            if (p301State.value == 3)
+                tienTong.value = checkLastest(tong301)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p301State.value == 3) tienTong.value =
+                checkLastest(tong301)
+            else tienTong.value = 301
+        }
+        return tienTong
+    }
+
+    fun getTienTong302(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong302) { value ->
+            if (p302State.value == 3)
+                tienTong.value = checkLastest(tong302)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p302State.value == 3) tienTong.value =
+                checkLastest(tong302)
+            else tienTong.value = 302
+        }
+        return tienTong
+    }
+
+
+    fun getTienTong303(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong303) { value ->
+            if (p303State.value == 3)
+                tienTong.value = checkLastest(tong303)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p303State.value == 3) tienTong.value =
+                checkLastest(tong303)
+            else tienTong.value = 303
+        }
+        return tienTong
+    }
+
+
+    fun getTienTong304(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong304) { value ->
+            if (p304State.value == 3)
+                tienTong.value = checkLastest(tong304)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p304State.value == 3) tienTong.value =
+                checkLastest(tong304)
+            else tienTong.value = 304
+        }
+        return tienTong
+    }
+
+
+    fun getTienTong305(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong305) { value ->
+            if (p305State.value == 3)
+                tienTong.value = checkLastest(tong305)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p305State.value == 3) tienTong.value =
+                checkLastest(tong305)
+            else tienTong.value = 305
+        }
+        return tienTong
+    }
+
+
+    fun getTienTong401(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong401) { value ->
+            if (p401State.value == 3)
+                tienTong.value = checkLastest(tong401)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p401State.value == 3) tienTong.value =
+                checkLastest(tong401)
+            else tienTong.value = 401
+        }
+        return tienTong
+    }
+
+    fun getTienTong402(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong402) { value ->
+            if (p402State.value == 3)
+            tienTong.value = checkLastest(tong402)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p402State.value == 3) tienTong.value =
+                checkLastest(tong402)
+            else tienTong.value = 402
+        }
+        return tienTong
+    }
+
+
+    fun getTienTong403(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong403) { value ->
+            if (p403State.value == 3)
+            tienTong.value = checkLastest(tong403)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p403State.value == 3) tienTong.value =
+                checkLastest(tong403)
+            else tienTong.value = 403
+        }
+        return tienTong
+    }
+
+
+    fun getTienTong404(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong404) { value ->
+            if (p404State.value == 3)
+            tienTong.value = checkLastest(tong404)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p401State.value == 3) tienTong.value =
+                checkLastest(tong404)
+            else tienTong.value = 404
+        }
+        return tienTong
+    }
+
+
+    fun getTienTong405(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong405) { value ->
+            if (p405State.value == 3)
+            tienTong.value = checkLastest(tong405)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p405State.value == 3) tienTong.value =
+                checkLastest(tong405)
+            else tienTong.value = 405
+        }
+        return tienTong
+    }
+
+
+    fun getTienTong501(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong501) { value ->
+            if (p501State.value == 3)
+            tienTong.value = checkLastest(tong501)
+        }
+        tienTong.addSource(checkDebt) { value ->
+
+            if (checkDebt.value == true && p501State.value == 3) tienTong.value =
+                checkLastest(tong501)
+            else tienTong.value = 501
+        }
+        return tienTong
+    }
+
+    fun getTienTong502(): LiveData<Int> {
+        val tienTong = MediatorLiveData<Int>()
+        tienTong.addSource(tong502) { value ->
+            if (p502State.value == 3)
+            tienTong.value = checkLastest(tong502)
+        }
+        tienTong.addSource(checkDebt) { value ->
+            if (checkDebt.value == true && p502State.value == 3) tienTong.value =
+                checkLastest(tong502)
+            else tienTong.value = 502
+        }
+        return tienTong
+    }
+
+
+    val tong101Real = getTienTong101()
+    val tong101String = Transformations.map(tong101Real) { value ->
+        value.toString()
+    }
+
+    val tong102Real = getTienTong102()
+    val tong102String = Transformations.map(tong102Real) { value ->
+        value.toString()
+    }
+
+    val tong103Real = getTienTong103()
+    val tong103String = Transformations.map(tong103Real) { value ->
+        value.toString()
+    }
+
+    val tong104Real = getTienTong104()
+    val tong104String = Transformations.map(tong104Real) { value ->
+        value.toString()
+    }
+
+    val tong105Real = getTienTong105()
+    val tong105String = Transformations.map(tong105Real) { value ->
+        value.toString()
+    }
+
+    val tong201Real = getTienTong201()
+    val tong201String = Transformations.map(tong201Real) { value ->
+        value.toString()
+    }
+    val tong202Real = getTienTong202()
+    val tong202String = Transformations.map(tong202Real) { value ->
+        value.toString()
+    }
+
+    val tong203Real = getTienTong203()
+    val tong203String = Transformations.map(tong203Real) { value ->
+        value.toString()
+    }
+
+    val tong204Real = getTienTong204()
+    val tong204String = Transformations.map(tong204Real) { value ->
+        value.toString()
+    }
+
+    val tong205Real = getTienTong205()
+    val tong205String = Transformations.map(tong205Real) { value ->
+        value.toString()
+    }
+
+    val tong301Real = getTienTong301()
+    val tong301String = Transformations.map(tong301Real) { value ->
+        value.toString()
+    }
+    val tong302Real = getTienTong302()
+    val tong302String = Transformations.map(tong302Real) { value ->
+        value.toString()
+    }
+    val tong303Real = getTienTong303()
+    val tong303String = Transformations.map(tong303Real) { value ->
+        value.toString()
+    }
+    val tong304Real = getTienTong304()
+    val tong304String = Transformations.map(tong304Real) { value ->
+        value.toString()
+    }
+    val tong305Real = getTienTong305()
+    val tong305String = Transformations.map(tong305Real) { value ->
+        value.toString()
+    }
+
+
+    val tong401Real = getTienTong401()
+    val tong401String = Transformations.map(tong401Real) { value ->
+        value.toString()
+    }
+
+    val tong402Real = getTienTong402()
+    val tong402String = Transformations.map(tong402Real) { value ->
+        value.toString()
+    }
+
+    val tong403Real = getTienTong403()
+    val tong403String = Transformations.map(tong403Real) { value ->
+        value.toString()
+    }
+
+    val tong404Real = getTienTong404()
+    val tong404tring = Transformations.map(tong404Real) { value ->
+        value.toString()
+    }
+
+    val tong405Real = getTienTong405()
+    val tong405String = Transformations.map(tong405Real) { value ->
+        value.toString()
+    }
+
+    val tong501Real = getTienTong501()
+    val tong501String = Transformations.map(tong501Real) { value ->
+        value.toString()
+    }
+
+    val tong502Real = getTienTong502()
+    val tong502String = Transformations.map(tong502Real) { value ->
+        value.toString()
+    }
+
+
+    fun longPress(key: Int, state: Int) {
         uiScope.launch {
             update(key, state)
         }
@@ -68,6 +527,7 @@ class TitleViewModel(
     fun mapTag(p: Int): Int =
         when (p) {
             0 -> 0
+            3 -> 3
             else -> 1
         }
 
@@ -121,11 +581,19 @@ class TitleViewModel(
     fun radio5Checked() {
         _radioChecked.value = 5
     }
-    //endregion
+//endregion
 
 
     init {
+        checkDebt.value = false
         _radioChecked.value = 0
+    }
+
+    fun rotate() {
+        GlobalScope.launch(Dispatchers.Main) {
+            if (checkDebt.value == true) checkDebt.value = false
+            else if (checkDebt.value == false) checkDebt.value = true
+        }
     }
 
     private val _maPhong = MutableLiveData<Int>()
@@ -148,6 +616,7 @@ class TitleViewModel(
     fun doneNavigate() {
         _check.value = -1
     }
+
 
     override fun onCleared() {
         super.onCleared()
